d075f6589032a932d2b53de14b9b86b36f6826fa
adnan ali <adnanali11875@gmail.com>
2025-03-05 19:20:41 +0530

    hid the supabase config

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://wxlmrtitzpcoqqkicsai.supabase.co"; 
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind4bG1ydGl0enBjb3Fxa2ljc2FpIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczODkyOTUzOSwiZXhwIjoyMDU0NTA1NTM5fQ.csCx5ePq7K-BI6U7oDyvs8vWtwqZGx1e3X-zOP5x0uw";
+import { configDotenv } from "dotenv";
+const supabaseUrl = process.env.supabaseUrl
+const supabaseKey = process.env.supabaseKey
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
